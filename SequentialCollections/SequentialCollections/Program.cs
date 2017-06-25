c61019a9e36b40a15c5f26d24210701e8f355b7d
Szilniczky <szilniczky.david@gmail.com>
2017-06-25 18:04:55 +0200

    Codes added

---
+++ SequentialCollections/SequentialCollections/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SequentialCollections
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Queue");
+            Queue queue = new Queue();
+            queue.Enqueue("First");
+            queue.Enqueue("Second");
+            queue.Enqueue("Third");
+            queue.Enqueue("Fourth");
+
+            while (queue.Count > 0)
+            {
+                object obj = queue.Dequeue();
+
+                Console.WriteLine("From Queue: {0}", obj);
+            }
+
+            Console.WriteLine("Stack");
+            Stack stack = new Stack();
+            stack.Push("First");
+            stack.Push("Second");
+            stack.Push("Third");
+            stack.Push("Fourth");
+
+            while (stack.Count > 0)
+            {
+                object obj = stack.Pop();
+                Console.WriteLine("From Stack: {0}", obj);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
